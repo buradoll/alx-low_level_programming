6dd7848c06716ce2e80aec62598946959a6f6297
buradoll <birukkam@gmail.com>
2021-08-29 08:35:25 -0700

    check betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include <stdlib.h>
-
+/*lower and upper case*/
 int main(void)
 {
 int i;
-
-for (i = 9; i <= 122; i++)
+/*print alphabet with lower case*/
+for (i = 97; i <= 122; i++)
 putchar(i);
+/*print alphabet with Upper case*/
 for (i = 65; i <= 90; i++)
 putchar(i);
+/*newline*/
 putchar(10);
 }
