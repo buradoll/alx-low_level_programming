b12fe93b989f9633a342370f45e583b1ab2b4755
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 12:38:45 -0700

    Create 2-print_alphabet_x10.c
    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+/**
+* main - Short description, single line
+* Return: Description of the returned value
+*/
+void print_alphabet_x10(void);
+int main(void)
+{    
+void print_alphabet_x10(void);
+{
+int i = 0;
+int j = 0;
+
+while (j<10)
+{
+for (i = 97; i < 123; i++)
+putchar (i);
+putchar (10);
+j++;
+}
+}
+return(1);
+}
