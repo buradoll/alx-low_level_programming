2ed37d7e0ba6507def8fb5b165d3d42c15cdf0a8
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 12:04:44 +0300

    Update 4-print_alphabt.c
    update from remote because of network problem

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,20 @@ int i;
 * for loop depend on their ascii code to separate
 */
 for (i = 97; i <= 122; i++)
+/**
+* for loop depend on their ascii code to separate
+*/
 if (i != 101 && i != 113)
+/**
+* print using putchar
+*/
 putchar (i);
+/**
+* print using putchar
+*/
 putchar (10);
+/**
+* return value
+*/
 return (0);
 }
