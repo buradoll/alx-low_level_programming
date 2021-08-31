737d54aac2c46ba1d8508e2471a20d871838910d
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 11:18:51 -0700

    Update 1-alphabet.c
    compare

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 # include "main.h"
 # include <stdio.h>
-void print_alphabet(void);
 /**
 * main - Short description, single line
 * Return: Description of the returned value
 */
 int main(void)
 {
-int i;
-
 print_alphabet(void)
 {
+int i;
+
 for (i = 97; i < 122; i++)
 putchar (i);
 }
