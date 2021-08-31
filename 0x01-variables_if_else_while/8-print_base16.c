8eb0d2abac865d567711ba320b66f7b98bb66127
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 15:00:47 +0300

    Update 8-print_base16.c
    network problem make me stuck that why

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Short description, single line
+* Return: Description of the returned value
+*/
+int main(void)
+{
+int i = 0;
+
+for (i = 48; i <= 57; i++)
+putchar(i);
+for (i = 65; i <= 70; i++)
+putchar (i);
+putchar (10);
+return(0);
+}
