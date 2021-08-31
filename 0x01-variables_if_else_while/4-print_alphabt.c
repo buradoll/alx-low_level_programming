ef8a1a1b6c257a3f9f82622f5db5e84f44eaf613
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 13:50:59 +0300

    Update 4-print_alphabt.c
    check

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
-* for loop depend on their ascii code to separate
+* main - Short description, single line
+(* section header: Section description)*
+* Return: Description of the returned value
 */
 int main(void)
 {
@@ -13,9 +15,21 @@ int i;
 * @i++ for hexadecimal
 *for loop depend on their ascii code to separate
 */
+/**
+* for - Check if a number is greater than 0
+* @i: The number to be checked
+*
+* Return: 1 if the number is positive. 0 otherwise
+*/
 for (i = 97; i <= 122; i++)
 /**
 * for loop depend on their ascii code to separate
+*/
+/**
+* if - Check if a number is greater than 0
+* @i: The number to be checked
+*
+* Return: 1 if the number is positive. 0 otherwise
 */
 if (i != 101 && i != 113)
 /**
