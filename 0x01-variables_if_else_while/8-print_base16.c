2a957dcab97e4602403d88ab2c1dc91e6cfdec8c
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 15:06:12 +0300

    Update 8-print_base16.c
    check for code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,13 @@
 * main - Short description, single line
 * Return: Description of the returned value
 */
-int main(void)
+int main (void)
 {
 int i = 0;
 
 for (i = 48; i <= 57; i++)
 putchar(i);
-for (i = 65; i <= 70; i++)
+for (i = 97; i <= 102; i++)
 putchar (i);
 putchar (10);
 return(0);
