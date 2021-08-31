15ebe9e3f7307e9ee7428c48ada1abde9cde077f
buradoll <88479359+buradoll@users.noreply.github.com>
2021-08-31 16:20:39 +0300

    Create 0-holberton.c
    the first one

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+# include <stdio>
+/**
+* main - Short description, single line
+* Return: Description of the returned value
+*/
+int main (void)
+{
+int i;
+char *alx = "Holberton";
+
+for (i = 0; i < 15; i++)
+putchar(alx[i]);
+putchar(10);
+}
