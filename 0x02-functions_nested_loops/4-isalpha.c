9702fda6e986a96ad6947e76667d65bbfbaf1ec3
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-01 13:02:12 -0700

    Create 4-isalpha.c
    only return

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+# include <main.h>
+# include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ * Return: void.
+ */
+int main(void)
+{
+int c;
+_isalpha(c);
+}
+int _isalpha (int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
