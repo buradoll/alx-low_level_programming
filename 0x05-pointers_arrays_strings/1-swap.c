7addc16cd50878ce8d7d13247532a3890b478159
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-07 16:34:55 +0300

    Update 1-swap.c
    betty style

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,14 @@
 /**
  * swap_int - check the code
  *@t save value of a
+ *@a main variable
+ *@b main variable
  *@g save value of b
  */
 void swap_int(int *a, int *b)
 {
-int t =*a;
-int g =*b;
+int t = *a;
+int g = *b;
 *b = t;
-*a= g;
+*a = g;
 }
