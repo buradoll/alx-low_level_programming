3db5528782d7f9e63f1bce8416a11aab1b2ec102
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-07 16:39:09 +0300

    Update 1-swap.c
    docs

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 /**
  * swap_int - check the code
- *@t save value of a
- *@a main variable
- *@b main variable
+ *@t: save value of a
+ *@a: main variable
+ *@b: main variable
  *@g save value of b
  */
 void swap_int(int *a, int *b)
