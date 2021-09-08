d9b404738afe3a915e8a19227898b9faf7dd8397
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 11:26:39 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,7 @@
  *_puts - check the code
  *_putchar - check the code
  * @c: string parameter
+ * @str: string parameter
  * Return: no return
  */
 void print_rev(char *str);
@@ -9,14 +10,13 @@ int _putchar (char c);
 void print_rev(char *str)
 {
 int len = 0;
-  
+
 while (*(str + len))
 len++;
 while (len >= 0)
 {
 /**
  *_putchar - check the code
- * @str: string parameter
  * Return: no return
  */
 _putchar (str[len]);
