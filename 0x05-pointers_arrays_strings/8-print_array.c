1710e96a00d80be388188f1d737e1827e1c36149
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 17:41:14 +0300

    Create 8-print_array.c
    check

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+/**
+ *_puts - check the code
+ *_putchar - check the code
+ * @c: string parameter
+ *_puts - check the code
+ * Return: no return
+ */
+int _putchar (char c);
+void print_array(int *a, int n)
+{
+int i = 0;
+
+n= 20/4;
+while(i <= n)
+{
+printf("%d",a[i]);
+printf(", ");
+i++;
+}
+printf("\n");
+printf("i value: %d", i);
+ 
+}
