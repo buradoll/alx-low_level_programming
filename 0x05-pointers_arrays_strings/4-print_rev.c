f96986aea4be8b9a140b7f2afea224e8961c9efc
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 11:28:21 +0300

    Update 4-print_rev.c
    doc

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,6 @@
 /**
- *_puts - check the code
+ *print_rev - check the code
  *_putchar - check the code
- * @c: string parameter
  * @str: string parameter
  * Return: no return
  */
@@ -18,6 +17,7 @@ while (len >= 0)
 /**
  *_putchar - check the code
  * Return: no return
+ * @str: string parameter
  */
 _putchar (str[len]);
 len--;
