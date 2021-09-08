cfc54853d3aea2bd06a45c81234f33c27e52c0da
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 16:51:57 +0300

    Create 7-puts_half.c
    check once

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+/**
+ *puts2 - check the code
+ *_putchar - check the code
+ * @str: string parameter
+ * Return: no return
+ */
+void puts2(char *str);
+int _putchar (char c);
+void puts2(char *str)
+{
+int len = 0;
+int i;
+
+while (*(str + len) != '\0')
+len++;
+i = len / 2; 
+while (i < len)
+{
+/**
+ *_putchar - check the code
+ * Return: no return
+ * @str: string parameter
+ */
+_putchar (str[i]);
+i++;
+i++;
+}
+_putchar ('\n');
+}
