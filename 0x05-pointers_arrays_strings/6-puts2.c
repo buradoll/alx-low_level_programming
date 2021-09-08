8317826906734d28e2b51ff69c82abf1d04defe7
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 16:44:43 +0300

    Create 6-puts2.c
    check for first

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+/**
+ *puts2 - check the code
+ *_putchar - check the code
+ * @str: string parameter
+ * Return: no return
+ */
+void puts2(char *str);
+int _putchar (char c);
+void puts2(char *str)
+{
+int len = 0;
+int i;
+
+while (*(str + len) != '\0')
+len++;
+len--;
+i=0;
+while (i > len)
+{
+/**
+ *_putchar - check the code
+ * Return: no return
+ * @str: string parameter
+ */
+_putchar (str[i]);
+i++;
+i++;
+}
+_putchar ('\n');
+}
