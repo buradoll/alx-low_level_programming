2186d296719fb589a3f0304fd227782231b5acea
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 10:36:33 +0300

    Create 4-print_rev.c
    try this

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+/**
+ *_puts - check the code
+ *_putchar - check the code
+ * @c: string parameter
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+int i;
+int len = 0;
+  
+while (*(str + len))
+len++;
+for (; len <= 0; len--)
+{
+/**
+ *_putchar - check the code
+ * @str: string parameter
+ * Return: no return
+ */
+_putchar (str[len]);
+}
+_putchar ('\n');
+}
+
+
+
+}
