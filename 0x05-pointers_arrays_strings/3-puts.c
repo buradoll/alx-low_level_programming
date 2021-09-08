ffc2090e8dbc93c45498031b20c3bee5a7cba316
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 09:05:17 +0300

    Update 3-puts.c
    to fix implicit prob

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,16 @@
  * @str: string parameter
  * @l: integer for count length of string 
  */
+int _putchar (char c);
 void _puts(char *str)
 {
 int len = 0;
 while (*(str + len))
 len++;
-while (len > 0)
+while (len >= 0)
 {
-_putchar(str[len]);
+_putchar (str[len]);
 len--;
 }
+_putchar (\n + '0');
 }
