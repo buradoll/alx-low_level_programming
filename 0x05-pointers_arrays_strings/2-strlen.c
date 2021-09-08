b3e24f7f92cef299b30c63794e213c8bd1397569
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 08:11:24 +0300

    Update 2-strlen.c
    betty check

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 /**
  * _strlen - function for length of string
  *@len length of string
- *@s- string parameter
+ *@s string parameter
  * Return: return len.
  */
 int _strlen(char *s)
 {
 int len = 0;
 
-while(*(s+len))
+while (*(s + len))
 len++;
-return(len);
+return (len);
 }
