d49cb9964750fbc5812b119007b58414258dcfa1
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 10:29:03 +0300

    Update 3-puts.c
    betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,8 +3,7 @@
  *_putchar - check the code
  * @c: string parameter
  *_puts - check the code
- * @str: string parameter
- * @len: integer for count length of string
+ * Return: no return
  */
 int _putchar (char c);
 void _puts(char *str)
