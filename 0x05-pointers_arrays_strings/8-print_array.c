09b2f0eaa17366e2bcb4f5bac53cac3a38bb1fca
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-08 14:57:59 -0700

    Update 8-print_array.c
    betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,8 +4,6 @@
  *_putchar - check the code
  *printf - check the code
  * @c: string parameter
- * @a: string parameter
- * @n: string parameter
  * Return: no return
  */
 int _putchar (char c);
@@ -15,12 +13,12 @@ int i = 0;
 
 while (i < n)
 {
-printf ("%d", a[i]);
+printf("%d", a[i]);
 if ((i + 1) < n)
-printf (", ");
+printf(", ");
 else
 break;
 i++;
 }
-printf ("\n");
+printf("\n");
 }
