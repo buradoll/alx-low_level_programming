f4e3427600e8f464b076f71c43dd6b24b52f222c
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-09 14:01:17 -0700

    Update 3-strcmp.c
    check

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,22 @@
 /**
- * main - check the code
+ * _strcmp - check the code
  *
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
-
-while (*s1 != '\0' || *s2 != '\0') {
-if (*s1 == *s2) {
-s1++;
-s2++;
-}
-else if ((*s1 == '\0' && *s2 != '\0')
-|| (*s1 != '\0' && *s2 == '\0') || (*s1 != *s2)
+int f = 0;
+int i = 0;
+int g;
+while (s1[i] != '\0' || s2[i] != '\0')
 {
-flag = 1;
+if (s1[i] == s2[i])
+{
+f=0;
+}
+else if (s1[i] != s2[i])
+f = s1[i] - s2[i];
+g += f;
+return f;
 }
 }
