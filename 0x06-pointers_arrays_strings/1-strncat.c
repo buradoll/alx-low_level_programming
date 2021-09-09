c9df12a2e2ae494d0287560012271ba123e75693
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-09 13:32:37 +0300

    Update 1-strncat.c
    check for real

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,21 +5,23 @@
  * @src: The character to print
  * Return: return dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src, int n)
 {
-int l =0;
-int i =0;
-int li;
-int g;
-int m = 0;
-while (*(dest + l) != '\0')
+int l =0; int i =0; int li; int g; int m = 0;
+while (*(dest + l))
 l++;
-l--;
+
 while (*(src + i) != '\0')
 i++;
 i--;
 li = i + l;
-while(m<=i)
+while(m < n)
+{
+dest[l+m] = src[m];
+m++;
+}
+l++;
+while(m <= i)
 {
 dest[l+m] = src[m];
 m++;
