acbd4f5659c43b8b35091b62a078c310f2d66755
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-09 12:03:08 -0700

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,15 @@
 /**
  * _strncpy - check the code
- *
+ *@dest: parameter
+ *@src: parameter
+ *@n: parameter
  * Return: return dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
+for (i = 0;i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 for ( ; i < n; i++)
 dest[i] = '\0';
