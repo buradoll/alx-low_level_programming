d02eacb33c971c75b2d66fb902a932c5a10d9744
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-09 15:25:04 +0300

    Create 2-strncpy.c
    check for real

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+/**
+ * _strncpy - check the code
+ *
+ * Return: return dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return dest;
+}
