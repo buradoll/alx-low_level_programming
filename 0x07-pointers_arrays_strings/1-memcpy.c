34edb512f3dce5a1015c6b836e0db9cde684fdc3
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-13 14:07:31 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,12 +2,11 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: Nothing.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i =0;
+int i = 0;
 char b;
 
 while (i < n)
