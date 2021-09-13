f0b61af065b386242740094e04e4c6da3297aaeb
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-13 11:38:38 +0300

    Create 1-memcpy.c
    first one

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i =0;
+char b;
+
+while (i < n)
+{
+
+if (sizeof(dest) == sizeof(src))
+{
+b = src[i];
+dest[i] = b;
+}
+i++;
+}
+}
