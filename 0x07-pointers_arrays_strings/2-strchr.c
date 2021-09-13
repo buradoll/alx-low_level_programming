cd60f50b6eca1f47e171b387cf84f73eb5ec1c98
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-13 10:17:31 -0700

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,13 +2,17 @@
  * _strchr - check the code
  * @s: parameter 
  * @c: parameter
+ *
  * Return: pointer of the occurence char.
  */
 char *_strchr(char *s, char c)
 {
 int i = 0;
+int h = 0;
 
-while(i <= sizeof(s))
+while (*(s + h))
+h++;
+while(i <= h)
 {
 if (s[i] == c)
 {
