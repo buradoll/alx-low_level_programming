f6c38b1160c32261052606b2b50db03aa22606db
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-13 10:08:35 +0300

    Create 0-memset.c
    first one

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * _memset - prints buffer in hexa
+ * @b: filled var
+ * @s: address for fill
+ * @n: bytes of filled
+ *
+ * Return: *s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int bu = 0;
+while (bu < n)
+{
+*(s + bu) = b;
+bu++;
+}
+return (*s);
+}
