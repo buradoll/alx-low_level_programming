ed79d04dd66a1500d56acb71452e407cbcc8ea4b
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-14 14:28:24 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,22 @@ char *_strchr(char *s, char c)
 {
 int i = 0;
 int h = 0;
+char ret;
 
 while (*(s + h))
 h++;
-while (i <= h)
+while (i < h)
 {
 if (s[i] == c)
 {
-return (s[i]);
+ret = s[i];
 break;
+}
+else
+{
+ret = NULL;
 }
 i++;
 }
+  return (ret);
 }
