7eca62f085bedad27be010ad2c25fbc1c4111116
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-14 13:37:10 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,9 +9,13 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 unsigned int i = 0;
 char b;
 
+while(*(dest+l))
+l++;
+while(*(src+k))
+k++;
 while (i < n)
 {
-if (sizeof(dest) == sizeof(src))
+if (k == l)
 {
 b = src[i];
 dest[i] = b;
