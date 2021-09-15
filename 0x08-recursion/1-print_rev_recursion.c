da692be4984eea165ce05c1a546bc3081d1568ef
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-15 13:38:42 -0700

    Update 1-print_rev_recursion.c
    check

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,20 @@
 /**
- * _puts_recursion - check the code
+ * _print_rev_recursion - check the code
  * @c: string for print
  * Return: void.
  */
 int _putchar(char c);
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-char b;
-b = *s;
-
-if (*s != '\0')
+int i = 0;
+char *b;
+if (i == 0)
+while(*(s + i))
+i++;
+i--;
+if (i >= 0)
 {
-_putchar(b);
-_puts_recursion();
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
