cc3c0370e2405d0cd13ea10023f4b7cc6d1ffbf8
buradoll <birukkam@gmail.com>
2021-09-16 16:09:38 +0300

    colapsed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints buffer in hexa
  * @argv: pointer array of argument
@@ -12,12 +14,18 @@ int j = 0;
 if (argc < 1)
 printf("0\n");
 if (argc > 0)
-for (i = 1; i <= argc; i++)
+for (i = 1; i < argc; i++)
+{
 if (atoi(argv[i]))
-j += atoi(argv[i]);
+{
+j = j + atoi(argv[i]);
+}
 else
 {
 printf("Error\n");
 return (1);
 }
+}
+printf("%d\n", j);
+return (0);
 }
