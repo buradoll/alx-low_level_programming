19d34ca79690a103f2ce6565d60591d24a8c1ec8
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-17 13:54:43 +0300

    Create 0-whatsmyname.c
    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints buffer in hexa
+ * @argv: pointer array of argument
+ * @argc: length of argument
+ *
+ * Return: No return.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
