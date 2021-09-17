491e772225598b16e40ec5bfc33c55d46d397a01
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-17 13:59:36 +0300

    Create 1-args.c
    check later

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints buffer in hexa
+ * @argv: pointer array of argument
+ * @argc: length of argument
+ *
+ * Return: No return.
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
