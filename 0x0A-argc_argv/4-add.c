e2014e3c6c0e2fa06acb3f0e00b5af964cf69c5d
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-17 14:18:01 +0300

    Create 4-add.c
    check later

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+/**
+ * main - prints buffer in hexa
+ * @argv: pointer array of argument
+ * @argc: length of argument
+ * Return: return 0.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int j = 0;
+
+if (argc < 1)
+printf("0\n");
+if (argc > 0)
+for (i = 1; i <= argc; i++)
+if (atoi(argv[i]))
+j += atoi(argv[i]);
+else
+{
+printf("Error\n");
+return (1);
+}
+}
