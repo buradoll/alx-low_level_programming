c758b27993396252e2edc1aee104019bda599e10
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-17 14:04:22 +0300

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+/**
+ * main - prints buffer in hexa
+ * @argv: pointer array of argument
+ * @argc: length of argument
+ *
+ * Return: No return.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i <= argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
