feeaac75d7338805f3567b5018a38755ece383ec
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-20 14:07:25 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,6 @@
  * main - prints buffer in hexa
  * @argv: pointer array of argument
  * @argc: length of argument
- *
  * Return: No return.
  */
 int main(int argc, __attribute__((unused)) char *argv[])
