3d576bb5a33782df53a7f4bbeb8ebcba623ee431
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-22 10:54:41 +0300

    Create 2-str_concat.c
    check please

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+/**
+ * str_concat - prints buffer in hexa
+ * @s1: the address of memory to concatnate
+ * @s2: the address of memory to concatnate
+ * Return: pointer of a.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+char *str_concat(char *s1, char *s2)
+{
+char *a;
+unsigned int j = 0;
+unsigned int i = 0;
+unsigned int y = 0;
+unsigned int x = 0;
+unsigned int s;
+
+while (*(s1 + j))
+i++;
+i--;
+while (*(s2 + j))
+j++;
+s = i + j;
+a = malloc(sizeof(char) * s);
+if (s1 == NULL || s2 == NULL)
+return (NULL);
+if (a == NULL)
+return (NULL);
+while (x < sizeof(s1))
+{
+a[x] = str[x];
+x++;
+}
+while (y <= sizeof(s2))
+{
+a[x] = str[y];
+x++;
+}
+return (a);
+}
