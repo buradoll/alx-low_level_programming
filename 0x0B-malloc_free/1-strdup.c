016b8ce19bf39077c42fb2e3244a4fd6e74fe340
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-22 09:11:48 +0300

    Create 1-strdup.c
    check

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+/**
+ * create_array - prints buffer in hexa
+ * @size: the address of memory to print
+ * @c: the size of the memory to print
+ *
+ * Return: pointer of a.
+ */
+#include <stdlib.h>
+#include <stddef.h>
+char *_strdup(char *str);
+{
+char *a;
+
+if (str == NULL)
+return (NULL);
+a = malloc(sizeof(str));
+a = str;
+if (*a == NULL)
+return (NULL);
+return (a);
+}
