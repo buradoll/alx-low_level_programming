3abb917726bccdcf9a49b207ef9ee125f8b79be9
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-22 13:31:00 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,13 +23,17 @@ while (*(s2 + j))
 j++;
 s = i + j;
 a = malloc(sizeof(char) * s);
-if (s1 != NULL)
+if (s1 == NULL && s2 == NULL)
+return (0);
+if (s1 == NULL)
+return (s2);
+if (s2 == NULL)
+return (s1);
 while (x <= i)
 {
 a[x] = s1[x];
 x++;
 }
-if (s2 != NULL)
 while (y <= j)
 {
 a[x] = s2[y];
