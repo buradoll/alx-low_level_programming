0505ccf557398ab1bbef14b93d3346978f54f91b
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-22 09:19:31 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,16 @@
 char *_strdup(char *str)
 {
 char *a;
+unsigned int i = 0;
 
 if (str == NULL)
 return (NULL);
 a = malloc(sizeof(str));
-*a = str;
+while (i < sizeof(str))
+{
+*a[i] = str[i];
+i++;
+}
 if (a == NULL)
 return (NULL);
 return (a);
