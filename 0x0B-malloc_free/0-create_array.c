8bc113040aed8c80651c0afb7c5793e19fd00eba
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-21 19:30:27 -0700

    Create 0-create_array.c
    start here

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *a;
+
+i = 0;
+if (size == 0)
+return (NULL);
+a = malloc(size * sizeof(char));
+if (a == NULL);
+return (NULL);
+while (i < size)
+a[i] = c;
+free(a);
+return (a);
+}
