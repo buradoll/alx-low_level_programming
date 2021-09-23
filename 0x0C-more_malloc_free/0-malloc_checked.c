08ca7ef6b86f8b55bb0490e65e2c2b39ef8bc389
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-23 12:18:14 -0700

    Update 0-malloc_checked.c
    betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 /**
- * string_nconcat - check the code
- * @s1: char parameter
- * @s1: char parameter
- * @n: length of concat
+ * malloc_checked - check the code
+ * @b: length of concat
  * Return: malloc pointer.
  */
 void *malloc_checked(unsigned int b)
