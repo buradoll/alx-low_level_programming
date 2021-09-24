c50c26597be6109ccdc645d56355ff9ee287dc97
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-24 09:54:17 -0700

    Update 1-string_nconcat.c
    check

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,16 +23,16 @@ n = j;
 d = malloc(sizeof(char) * (i + n));
 if (d == NULL)
 return (NULL);
-if (*s1 != '\0')
+if (s1 != '\0')
 if (s1 != NULL && d != NULL)
 while (x < i)
 {
 d[x] = s1[x];
 x++;
 }
-if (*s2 != '\0')
+if (s2 != '\0')
 if (s2 != NULL && d != NULL)
-while (y < n)
+while (y <= n)
 {
 d[x] = s2[y];
 x++;
