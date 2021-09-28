8a53a2601dd8adabed524f0f832a6f906ca39796
buradoll <88479359+buradoll@users.noreply.github.com>
2021-09-28 12:06:49 -0700

    Create 1-init_dog.c
    check

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - check the code
+ *@d parameter.
+ *@name parameter.
+ *@age parameter.
+ *@owner parameter.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
